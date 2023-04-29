e601e8fca1dc0fb8c413b550cfef277dc75b6319
pardhiv9 <113880142+pardhiv9@users.noreply.github.com>
2023-04-29 14:16:34 +0530

    Add files via upload

---
+++ prog 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("enter the integer");
+	scanf("%d",&num);
+	if(num%2==0)
+	{
+		printf("even numer");
+	}
+	else
+	{
+		printf("odd number");
+	}
+	return 0;
+}
