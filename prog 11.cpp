c2ef1d084e1f76d2d14713b33ade26bb26e17d36
pardhiv9 <113880142+pardhiv9@users.noreply.github.com>
2023-05-06 08:46:37 +0530

    Add files via upload

---
+++ prog 11.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_SIZE 100
+
+int stack[MAX_SIZE];
+int top = -1;
+
+void push(int item);
+int pop();
+void delete();
+
+int main() {
+    // code for main function
+}
+
+void push(int item) {
+    // code for push function
+}
+
+int pop() {
+    // code for pop function
+}
+
+void delete() {
+    // code for delete function
+}
